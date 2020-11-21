dc081cff722af1de510bc8459469ccef7e3b1732
Arthur James <11641990+ArtiDjeims@users.noreply.github.com>
2020-11-21 17:02:36 +0200

    Updates

---
+++ precache-manifest.094dc9a01bcd1a0042f4080b7dbe48d6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcfde72e15c636631288dbead1dfe01c",
+    "url": "/static/index.html"
+  },
+  {
+    "revision": "32fc1eb21669a9b3dad4",
+    "url": "/static/static/css/2.79b87bb5.chunk.css"
+  },
+  {
+    "revision": "690cccea6c3a3e557db2",
+    "url": "/static/static/css/main.b293d7ef.chunk.css"
+  },
+  {
+    "revision": "32fc1eb21669a9b3dad4",
+    "url": "/static/static/js/2.0b43424f.chunk.js"
+  },
+  {
+    "revision": "02430e196a7794efc791c6af87b9b057",
+    "url": "/static/static/js/2.0b43424f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "690cccea6c3a3e557db2",
+    "url": "/static/static/js/main.6b004ad9.chunk.js"
+  },
+  {
+    "revision": "863c517dd1e4549ad27e",
+    "url": "/static/static/js/runtime-main.fd339aa1.js"
+  }
+]);
